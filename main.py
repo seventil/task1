84193877302493e336369db66d66cad988f9ad34
Ales Kutsepau <ales.kutepov@gmail.com>
2020-11-20 14:31:29 +0300

    main.py refactor: used inputCheck

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 # -*- coding: utf-8 -*-
-import json
+from inputCheck import InputHandler
 
-while True:
-    studentsPath = input("Plaese, enter a path to the students file: ")
-    try:
-        with open(studentsPath) as a:
-            students = json.loads(a.read())
-            break;
-    except IOError:
-        print("Students file is not accessible, try another path")
-        
-while True:  
-    roomsPath = input("Plaese, enter a path to the rooms file: ")
-    try:
-        with open(roomsPath) as b:
-            rooms = json.loads(b.read())
-            break;
-    except IOError:
-        print("Rooms file is not accessible, try another path")
-
-while True:    
-    outputFormat = input("Please, enter an output file format (XML or JSON): ").upper()  
-    if outputFormat.lower() == "xml" or outputFormat.lower() == "json":
-        break;
-    else:
-        print("This is not a XML or JSON file format . Please enter a valid format")
+ic = InputHandler()
+students = ic.getPath("students")
+rooms = ic.getPath("rooms")
+outputFormat = ic.getOutput()
+   
