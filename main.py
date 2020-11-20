f274b209191beffc346c5a2c0404a00e5424189c
Ales Kutsepau <ales.kutepov@gmail.com>
2020-11-20 18:34:52 +0300

    refactored main.py to use sys.argv with inputdatahandler

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-from inputCheck import InputHandler
+import sys
+from inputCheck import InitialDataHandler
 
-ic = InputHandler()
-students = ic.getPath("students")
-rooms = ic.getPath("rooms")
-outputFormat = ic.getOutput()
+#args = ["1","students.json","rooms.json","XML"]
+
+idh = InitialDataHandler(sys.argv)
+students = idh.getStudents()
+rooms = idh.getRooms()
+outputFormat = idh.getOutputFormat()
+
+
+print("outputFormat = ", outputFormat)
+print("rooms ", len(rooms))
+print("students ", len(students))
+print("finished")
    
