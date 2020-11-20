851d3a0b13eb28743c22941a3852b8edc13f8235
Ales Kutsepau <ales.kutepov@gmail.com>
2020-11-20 14:25:12 +0300

    added inputCheck.py that handles input data

---
+++ inputCheck.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class InputHandler():
+    def __init__(self):
+        pass
+    
+    def getPath(self, file):
+        while True:
+            path = input(f"Plaese, enter a path to the {file} file: ")
+            try:
+                with open(path) as a:
+                    jsonData = json.loads(a.read())
+                    break;
+            except IOError:
+                print(f"{file} file is not accessible, try another path")
+        return jsonData
+    
+    def getOutput(self):
+        while True:
+            outputFormat = input("Please, enter an output file format (XML or JSON): ").upper()  
+            if outputFormat.lower() == "xml" or outputFormat.lower() == "json":
+                break;
+            else:
+                print("This is not a XML or JSON file format . Please enter a valid format")
+        return outputFormat
+
+
