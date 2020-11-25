d23a1f338a93dff2dc8d59e33eac71acf2f9000d
seventil <seventil18@gmail.com>
2020-11-25 23:07:02 +0300

    update main.py
    added sys.exit functionality

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from file_dump import FileDump
 
 
 def main():
-    args = input_check.get_initial_args()
-    students = input_check.get_data_from_file("students", args.students)
-    rooms = input_check.get_data_from_file("rooms", args.rooms)
-    # output = input_check.check_output(args.output)
+	try:
+	    args = input_check.get_initial_args()
+	    students = input_check.get_data_from_file(args.students)
+	    rooms = input_check.get_data_from_file(args.rooms)
 
-    for room in rooms:
-        students_in_room = []
-        for student in students:
-            if student["room"] == room["id"]:
-                students_in_room.append({"id":student["id"], "name":student["name"]})
-        room["students"] = students_in_room
+	    for room in rooms:
+	        students_in_room = []
+	        for student in students:
+	            if student["room"] == room["id"]:
+	                students_in_room.append({"id":student["id"], "name":student["name"]})
+	        room["students"] = students_in_room
 
-    file_dump = FileDump(args.output)
-    file_dump.write_to_file(rooms)
-
-    print("all done!")
+	    file_dump = FileDump(args.output)
+	    file_dump.write_to_file(rooms)
+	except Exception:
+		return 1
+	else:
+		return 0
 
 
 if __name__ == "__main__":
-    main()
+	sys.exit(main())    
