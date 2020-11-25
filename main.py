0242709b7679f25bbfd75e0051df94c82f8d7838
seventil <seventil18@gmail.com>
2020-11-25 22:06:06 +0300

    update main.py
    refactored main to use new input_check.py

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 import sys
-import argparse
-from input_check import InputHandler
+import input_check
 from file_dump import FileDump
 
+
 def main():
-	parser = argparse.ArgumentParser(description="Join students and rooms files")
-	parser.add_argument("-s", "--students", default="students.json", help="Path to students file")
-	parser.add_argument("-r", "--rooms", default="rooms.json", help="Path to rooms file")
-	parser.add_argument("-o", "--output", help="Type of an output file")
-	args = parser.parse_args()
-	join(args)
+    args = input_check.get_initial_args()
+    students = input_check.checkData("students", args.students)
+    rooms = input_check.checkData("rooms", args.rooms)
+    output = input_check.checkOutput(args.output)
 
-def join(args):
-	input_handler = InputHandler()	
-	students = input_handler.getData("students", args.students)
-	rooms = input_handler.getData("rooms", args.rooms)
-	output = input_handler.getOutput(args.output)
+    for room in rooms:
+        students_in_room = []
+        for student in students:
+            if student["room"] == room["id"]:
+                students_in_room.append({"id":student["id"], "name":student["name"]})
+        room["students"] = students_in_room
 
-	for room in rooms:
-		students_in_room = []
-		for student in students:
-			if student["room"] == room["id"]:
-				students_in_room.append({"id":student["id"], "name":student["name"]})
-		room["students"] = students_in_room
-	    
-	file_dump = FileDump(output)
-	file_dump.write_to_file(rooms)
+    file_dump = FileDump(output)
+    file_dump.write_to_file(rooms)
+
+    print("all done!")
+
 
 if __name__ == "__main__":
-	main()
-	print("all done!")
+    main()
