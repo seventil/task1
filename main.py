82edac19fac3b9c8e2a3538906729211aa795a9e
seventil <seventil18@gmail.com>
2020-11-25 18:30:07 +0300

    update main.py
    refactored to use argument parser for sys.argv

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-# -*- coding: utf-8 -*-
 import sys
-from input_check import InitialDataHandler
+import argparse
+from input_check import InputHandler
 from file_dump import FileDump
 
-initial_data_handler = InitialDataHandler(sys.argv)
+def main():
+	parser = argparse.ArgumentParser(description="Join students and rooms files")
+	parser.add_argument("-s", "--students", default="students.json", help="Path to students file")
+	parser.add_argument("-r", "--rooms", default="rooms.json", help="Path to rooms file")
+	parser.add_argument("-o", "--output", help="Type of an output file")
+	args = parser.parse_args()
+	join(args)
 
-students = initial_data_handler.getStudents()
-rooms = initial_data_handler.getRooms()
-outputFormat = initial_data_handler.getOutputFormat()
+def join(args):
+	input_handler = InputHandler()	
+	students = input_handler.getData("students", args.students)
+	rooms = input_handler.getData("rooms", args.rooms)
+	output = input_handler.getOutput(args.output)
 
-for room in rooms:
-	students_in_room = []
-	for student in students:
-		if student["room"] == room["id"]:
-			students_in_room.append({"id":student["id"], "name":student["name"]})
-	room["students"] = students_in_room
-    
-file_dump = FileDump(outputFormat)
-file_dump.writeFile("output", rooms)
+	for room in rooms:
+		students_in_room = []
+		for student in students:
+			if student["room"] == room["id"]:
+				students_in_room.append({"id":student["id"], "name":student["name"]})
+		room["students"] = students_in_room
+	    
+	file_dump = FileDump(output)
+	file_dump.write_to_file(rooms)
 
-print("all done")
+if __name__ == "__main__":
+	main()
+	print("all done!")
