488230acb900a5159d09a45c64f2ecdc0a0ba26a
seventil <seventil18@gmail.com>
2020-11-25 18:30:39 +0300

    update input_check
    deleted deprecated class initialdatahandler

--- input_check.py
+++ input_check.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 import json
+
 
 class InputHandler():
 #Класс, который предназначен для обработки входных параметров. Проверит указан ли формат данных или путь
@@ -35,34 +35,3 @@ class InputHandler():
                 outputFormat = None
         return outputFormat
     
-    
-    
-class InitialDataHandler():
-#Класс, который должен при инициализации объекта должен обработать полученные на вход аргументы (например, sys.argv),
-#которые представляют собой последовательность: название файла, путь к студентам, путь к комнатам, название файла на выход
-#используя соответствующие методы, можно получить лист для студентов, лист для комнат и строку формат файла
-    def __init__(self, args):
-    #интересующие нас пераргументы изначально приравниваются к None или присваиваются из args, если они там указаны
-    #работает только со строгой последовательностью в args: [1] - путь к студентам, [2] - путь к комнатам,
-    #[3] - формат файла на выходе
-        self.studentPath = None
-        self.roomsPath = None
-        self.dataFormat = None
-        self.ih = InputHandler()
-        self.args = args
-        self.l = len(args)
-        if self.l > 1:
-            self.studentPath = self.args[1]
-        if self.l > 2:
-            self.roomsPath = self.args[2]
-        if self.l > 3:
-            self.dataFormat = self.args[3]
-                       
-    def getStudents(self):
-        return self.ih.getData("students", self.studentPath)
-    
-    def getRooms(self):
-        return self.ih.getData("rooms", self.roomsPath)
-    
-    def getOutputFormat(self):
-        return self.ih.getOutput(self.dataFormat)
