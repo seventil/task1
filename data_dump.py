876116f3becfcc008c66c4ab640298cbc00a6c61
Ales Kutsepau <ales.kutepov@gmail.com>
2020-11-29 12:54:52 +0300

    Update data_dump.py
    deleted deprecated write_to_file function

--- data_dump.py
+++ data_dump.py
@@ -64,27 +64,4 @@ class DataDumper():
     def dump_data(self, data):
         writer = self.writer_selector()
         writer.write(data)
-        
-    def write_to_file(self, data): 
-    # метод, записывающий в файл данные, переданные с data,
-    # в соответствии с заданными в объекте file_type и file_name       
-        if self.file_type.upper() == "JSON":
-            # сохранение в json файл
-            with open(self.file_name, "w") as file:
-                json.dump(data, file, indent = 2)                            
-        elif self.file_type.upper() == "XML":
-            # построение дерева для xml файла по строгой иерархии, с которой
-            # в метод передается data и последующее сохранение дерева в файл
-            root = ET.Element("root")
-            for room_data in data:     
-                room_branch = ET.SubElement(root, "room")
-                ET.SubElement(room_branch, "id").text = str(room_data["id"])
-                ET.SubElement(room_branch, "name").text = room_data["name"]
-                students_branch = ET.SubElement(room_branch, "students")
-                for students_data in room_data["students"]:
-                    stud = ET.SubElement(students_branch, "student")
-                    ET.SubElement(stud, "id").text = str(students_data["id"])
-                    ET.SubElement(stud, "name").text = students_data["name"]
-            tree = ET.ElementTree(root)
-            tree.write(self.file_name)
-            
+           
