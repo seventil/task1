874c52604b8ca224b0234632ff8b1bc9931d13d7
Ales Kutsepau <ales.kutepov@gmail.com>
2020-11-29 12:29:13 +0300

    refactor file_dump.py
    renamed module and FileDumper class, to reflect their full responsibility: data dumping. Created classes, that handle writing to files with different formats, as well as an interface that  handles interactions with objects of that classes. Inside of DataDumper created a method writer_selector, that returns an object of a specified writer, that corresponds to a file_type - a parameter that DataDumper takes in at init

--- data_dump.py
+++ data_dump.py
@@ -1,11 +1,47 @@
 import json
 import xml.etree.cElementTree as ET
+from abc import ABC, abstractmethod
 
 
-class FileDump():
+class Writer(ABC):
+# Интерфейс для взаимодействия класса FileDump с классами, записывающими данные
+# в файл, в бд и т.п. с соотв. типом данных: XML, JSON и т.д.
+    @abstractmethod
+    def write_to_file(self, data):
+        pass
+
+
+class JsonWriter(Writer):
+    def __init__(self, name):
+        self.name = name
+    def write_to_file(self, data):
+        with open(self.name, "w") as file:
+                json.dump(data, file, indent = 2)
+    
+    
+class XmlWriter(Writer):
+    def __init__(self, name):
+        self.name = name
+    def write_to_file(self, data):
+        # построение дерева для xml файла по строгой иерархии, с которой
+        # в метод передается data и последующее сохранение дерева в файл
+        root = ET.Element("root")
+        for room_data in data:     
+            room_branch = ET.SubElement(root, "room")
+            ET.SubElement(room_branch, "id").text = str(room_data["id"])
+            ET.SubElement(room_branch, "name").text = room_data["name"]
+            students_branch = ET.SubElement(room_branch, "students")
+            for students_data in room_data["students"]:
+                stud = ET.SubElement(students_branch, "student")
+                ET.SubElement(stud, "id").text = str(students_data["id"])
+                ET.SubElement(stud, "name").text = students_data["name"]
+        tree = ET.ElementTree(root)
+        tree.write(self.name) 
+
+
+class DataDumper():
 # Класс предназначен для того, чтобы записывать переданные данные в файл
 # в соответствии с установленным форматом и названием файла
-
     def __init__(self, file_type, file_name=None):
         self.file_type = file_type
         # Если при инициализации объекта не было задано имя,
@@ -18,6 +54,13 @@ class FileDump():
         # оно добавляется автоматически
         if self.file_type.lower() not in self.file_name:
             self.file_name += "." + file_type.lower()
+            
+    def writer_selector(self):
+    #метод, с помощью которого можно получить объект класса,
+    #осуществляющего запись данных с соответствующим 
+        DATA_DUMP_TYPES = {"JSON":JsonWriter(self.file_name), "XML":XmlWriter(self.file_name)}
+        return DATA_DUMP_TYPES[self.file_type]
+
        
     def write_to_file(self, data): 
     # метод, записывающий в файл данные, переданные с data,
@@ -41,3 +84,4 @@ class FileDump():
                     ET.SubElement(stud, "name").text = students_data["name"]
             tree = ET.ElementTree(root)
             tree.write(self.file_name)
+            
