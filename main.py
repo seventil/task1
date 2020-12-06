b79994eebed8067868a68222e7e6588646ea417d
Ales Kutsepau <ales.kutepov@gmail.com>
2020-12-06 23:12:24 +0300

    Update main.py
    rewrote main to solve task4 instead of task1, added 1st realised db query

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-import sys
-import input_check
+import input_handle
 from data_dump import DataDumper, WriterFactory
+import db_operations as dbo
 
 
 def main():
-    try:
-        args = input_check.get_initial_args()
-        students = input_check.get_data_from_file(args.students)
-        rooms = input_check.get_data_from_file(args.rooms)
 
-        for room in rooms:
-            students_in_room = []
-            for student in students:
-                if student["room"] == room["id"]:
-                    students_in_room.append({"id": student["id"],
-                                             "name": student["name"]})
-            room["students"] = students_in_room
+    # students.json rooms.json JSON
+    args = input_handle.get_initial_args()
+    students = input_handle.get_data_from_file(args.students)
+    rooms = input_handle.get_data_from_file(args.rooms)
 
-        dumper = DataDumper(WriterFactory())
-        dumper.dump_data(rooms, args.output, "output")
+    db_connection = dbo.DBConnection("localhost", "root", "root", "task4")
+    dbo.create_tables(db_connection, cleanse=True)
 
-    except Exception:
-        return 1
-    else:
-        return 0
+    dbo.insert_json_into_db(rooms, "rooms", db_connection)
+    dbo.insert_json_into_db(students, "students", db_connection)
+
+    solved_problems_results = {
+        'first_problem': dbo.get_json_from_query(dbo.QueryOrganizer(1), db_connection)
+
+
+    }
+
+    dumper = DataDumper(WriterFactory())
+    for name, data in solved_problems_results:
+        dumper.dump_data(data, args.output, name)
+
+
+    
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
